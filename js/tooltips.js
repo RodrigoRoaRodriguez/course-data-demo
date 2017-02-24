cfb398e77d8a563aa2a6b25fce9f2aea1b1302aa
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2017-02-24 08:07:41 +0100

    feat: on click events in preparation for tooltip

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,20 +1,29 @@
 class Tooltip {
   constructor(){
-    this.div = d3.select('body').append('div').attr('class', 'tooltip')
+    this.selection = d3.select('body').append('div').attr('class', 'tooltip')
     .classed('hidden', true)
     console.log('A Tooltip has been created!')
   }
 }
+Tooltip.prototype.hide = function(){
+    this.selection.classed('hidden', true)
+}
+Tooltip.prototype.show = function(){
+    this.selection.classed('hidden', false)
+}
+
 
 class TooltipedTreemap extends Treemap{
     constructor(rawData){
         super(rawData)
-        let tooltip = new Tooltip()
+        this.tooltip = new Tooltip()
     }
 }
 
 TooltipedTreemap.prototype.drawOnSVG = function(svg){
     Treemap.prototype.drawOnSVG.call(this, svg)
+    let tooltip = new Tooltip()
+    
     svg.selectAll('g.course')
-        // .on('click')_
+        .on('click', d=> console.log(d))
 }
